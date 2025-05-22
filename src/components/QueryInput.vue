a3f77f4b768af2a3f66f3e6537a8841aad6d0f86
s414june <s414june@gmail.com>
2025-05-22 22:21:08 +0800

    fix

--- src/components/QueryInput.vue
+++ src/components/QueryInput.vue
@@ -1,35 +1,19 @@
 <template>
-  <div
-    class="!fixed !w-full !bg-white style-isolated"
-    style="z-index: 1000"
-    :class="{ '!hidden': !isVisible }"
-  >
+  <div class="!fixed !w-full !bg-white style-isolated" style="z-index: 1000" :class="{ '!hidden': !isVisible }">
     <div class="!flex !items-center !gap-2 !w-full">
-      <input
-        v-model="url"
-        @focus="isFocused = true"
-        @blur="isFocused = false"
-        @keyup.enter="loadPage"
+      <input v-model="url" @focus="isFocused = true" @blur="isFocused = false" @keyup.enter="loadPage"
         placeholder="https://example.com"
-        class="!transition-opacity !duration-300 !border !border-zinc-300 !rounded !p-2 !text-sm !focus:outline-none !focus:ring-1 !focus:ring-blue-500 !flex-1"
-      />
+        class="!transition-opacity !duration-300 !border !border-zinc-300 !rounded !p-2 !text-sm !focus:outline-none !focus:ring-1 !focus:ring-blue-500 !flex-1" />
       <!-- 三點按鈕 -->
       <button @click.stop="toggleMenu" class="!px-2 !text-xl">⋯</button>
       <!-- 選單 -->
-      <div
-        v-if="showMenu"
-        class="!overflow-hidden !absolute !right-0 !top-full !mt-2 !w-40 !bg-white !shadow-lg !shadow !rounded !z-10 !text-sm"
-      >
-        <button
-          class="!block !border-none !w-full !text-left !px-4 !py-2 !hover:bg-zinc-100"
-          @click="openOriginalSite"
-        >
+      <div v-if="showMenu"
+        class="!overflow-hidden !absolute !right-0 !top-full !mt-2 !w-40 !bg-white !shadow-lg !shadow !rounded !z-10 !text-sm">
+        <button class="!block !border-none !w-full !text-left !px-4 !py-2 !hover:bg-zinc-100" @click="openOriginalSite">
           跳回原網頁
         </button>
-        <button
-          class="!block !border-none !w-full !text-left !px-4 !py-2 !hover:bg-zinc-100"
-          @click="emit('showEditor')"
-        >
+        <button class="!block !border-none !w-full !text-left !px-4 !py-2 !hover:bg-zinc-100"
+          @click="emit('showEditor')">
           編輯css樣式
         </button>
       </div>
@@ -80,7 +64,7 @@ function handleScroll() {
 function openOriginalSite() {
   if (url.value) {
     window.open(url.value, '_blank')
-    showMenu.value = false
+    emit('update:showMenu', false)
   }
 }
 
